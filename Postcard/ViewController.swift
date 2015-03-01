c781d7336d16f2ac817b491e1c2cef4ce1fe4f6d
Kristofor Vokes <kristofor@mym.ac>
2015-02-28 22:14:21 -0600

    Adding Challenge 1 items. Part of Challenge 2.

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var sendNameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,9 +30,13 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         //Send Mail button is pressed
+        sendNameLabel.text = enterNameTextField.text
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        sendNameLabel.textColor = UIColor.blueColor()
         messageLabel.hidden = false
+        sendNameLabel.hidden = false
+        enterNameTextField.text = ""
         enterMessageTextField.text = ""
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
